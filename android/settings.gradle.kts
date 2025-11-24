eb23c57624242eff08cccf550e55b4796cad41a7
centralglssk-arch <centralglssk@gmail.com>
2025-11-24 12:12:58 -0300

    fix: removed old flutter gradle plugin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -9,7 +9,6 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
 
 include(":app")
